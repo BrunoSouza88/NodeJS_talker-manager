fbc4b1b518c31e557bea00224ea2313f9c822f52
BrunoSouza <desouza88@hotmail.com>
2023-04-14 15:44:09 -0300

    Requisitos 01 e 02

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const fs = require('fs').promises;
+
 const app = express();
 app.use(express.json());
+
+const filePath = './src/talker.json';
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -12,6 +16,31 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log(`Online na porta ${PORT}`);
 });
 
+const readFile = async () => {
+  const file = await fs.readFile(filePath, 'utf-8');
+  const fileParsed = JSON.parse(file);
+  return fileParsed;
+};
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await readFile();
+
+  if (!talkers) {
+    return res.status(200).send([]);
+  } 
+    return res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  const talker = talkers.find((element) => Number(element.id) === Number(id));
+
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  } 
+    return res.status(200).json(talker);
+});
